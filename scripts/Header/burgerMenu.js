0a4cb500ec88dcba38a525d710b657bf0a24cf64
slavikrudenko <slavikrudenko@gmail.com>
2023-06-16 10:26:36 +0300

    change mobile menu of header

--- scripts/Header/burgerMenu.js
+++ scripts/Header/burgerMenu.js
@@ -2,10 +2,12 @@ const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".menu__button");
 const checkbox = document.querySelector(".menu__toggle");
 const menuBox = document.querySelector(".header__menu");
+const body = document.querySelector("body");
 
 const toggleMenu = () => {
   menuButton.classList.toggle("open");
   menuBox.classList.toggle("header__menu--open");
+  body.classList.toggle("overflow-hidden");
   checkbox.checked = !checkbox.checked;
 };
 
